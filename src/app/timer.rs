de344019740bade347e4631051183ca2af77d7a9
Marco Almeida <marcoantonio.aa09@gmail.com>
2025-04-30 18:24:08 -0300

    input to set sprint and breaks duration and formated output

--- src/app/timer.rs
+++ src/app/timer.rs
@@ -1,26 +1,36 @@
 use std::time::{Instant, Duration};
 use std::thread;
+use std::io;
 
-    pub fn sprint(){
+    pub fn timer(num: u64){
 
-    let duration = Duration::from_secs(25 * 60);
+    let duration = Duration::from_secs(num * 60);
     let start = Instant::now();
 
     while start.elapsed() < duration{
         let remaining = duration - start.elapsed();
-        println!("Time remaining: {:?}m", remaining);
+        let secs = remaining.as_secs();
+        let mins = secs / 60;
+        let remaining_secs = secs % 60;
+
+        println!("Time remaining: {:02}:{:02}", mins, remaining_secs);
         thread::sleep(Duration::from_secs(1));
     }
     }
 
-    pub fn break_time(){
-
-    let duration = Duration::from_secs(5 * 60);
-    let start = Instant::now();
-
-    while start.elapsed() < duration{
-        let remaining = duration - start.elapsed();
-        println!("Time remaining: {:?}m", remaining);
-        thread::sleep(Duration::from_secs(1));
-    }
+    pub fn user_choice() -> u64 {
+        loop{
+            println!("Set the time in minutes");
+    
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+    
+            match input.trim().parse::<u64>(){
+                Ok(time) => return time,
+                Err(_) =>{ 
+                    eprintln!("Error: Invalid Input!");
+                    continue;
+                }
+            }
+        }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod app;
 use std::io;
 
-use app::timer::{break_time, sprint};
+use app::timer::{timer, user_choice};
 fn main() {
     while true {
-
-        println!("1. Sprint (25 minutes)");
-        println!("2. Break (5 minutes)");
+        println!("1. Sprint");
+        println!("2. Break");
         println!("3. Exit");
         println!("Choose one option!");
 
@@ -14,10 +13,19 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
 
         match input.trim() {
-            "1" => sprint(),
-            "2" => break_time(),
+            "1" => sprint_choice(),
+            "2" => break_choice(),
             "3" => break,
             _ => println!("Please choose a valid option!"),
         }
     }
+}
+
+fn sprint_choice() {
+    let input = user_choice();
+    timer(input);
+}
+fn break_choice() {
+    let input = user_choice();
+    timer(input);
 }
